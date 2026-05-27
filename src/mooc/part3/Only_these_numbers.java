3c1f693380501648fb1988bbe87da077e59b83ad
SadiaRafique <sadia.rafique777@gmail.com>
2026-05-27 22:56:43 +0500

    Create Only_these_numbers.java

---
+++ src/mooc/part3/Only_these_numbers.java
@@ -0,0 +1,29 @@
+package mooc.part3;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Only_these_numbers {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            ArrayList<Integer> numbers = new ArrayList<>();
+            while (true) {
+                int number = Integer.valueOf(scanner.nextLine());
+                if (number == -1) {
+                    break;
+                }
+                numbers.add(number);
+            }
+
+            System.out.println("");
+
+            System.out.print("From where? ");
+            int start = Integer.valueOf(scanner.nextLine());
+
+            System.out.print("To where? ");
+            int end = Integer.valueOf(scanner.nextLine());
+
+            for (int i = start; i <= end; i++) {
+                System.out.println(numbers.get(i));
+            }
+        }
+    }
