893929db80664090a3a874609e824ac0881859a0
SadiaRafique <sadia.rafique777@gmail.com>
2026-06-05 00:19:21 +0500

    Update Index_of_smallest.java

--- src/mooc/part3/Index_of_smallest.java
+++ src/mooc/part3/Index_of_smallest.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class Index_of_smallest {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
         ArrayList<Integer> list = new ArrayList<>();
 
         while (true) {
-            int input = Integer.valueOf(scanner.nextLine());
+            int input = Integer.valueOf(sc.nextLine());
             if (input == 9999) {
                 break;
             }
