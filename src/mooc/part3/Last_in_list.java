367590db122e1280d64c17e3094468b8636cc4b2
SadiaRafique <sadia.rafique777@gmail.com>
2026-06-07 18:06:07 +0500

    Update Last_in_list.java

--- src/mooc/part3/Last_in_list.java
+++ src/mooc/part3/Last_in_list.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class Last_in_list {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         ArrayList<String> list = new ArrayList<>();
         while(true){
-            String name = input.nextLine();
+            String name = sc.nextLine();
             if(name.isEmpty()){
                 break;
             }else{
