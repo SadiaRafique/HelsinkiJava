a1239e507a78808fea33c3059a511e7c2a198128
SadiaRafique <sadia.rafique777@gmail.com>
2026-06-16 09:45:01 +0500

    Update SumofNumbers.java

--- src/mooc/part2/SumofNumbers.java
+++ src/mooc/part2/SumofNumbers.java
@@ -2,11 +2,11 @@ package mooc.part2;
 import java.util.Scanner;
 public class SumofNumbers {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sca = new Scanner(System.in);
         int sum = 0;
         while(true){
             System.out.println("Give a number:");
-            int num = Integer.valueOf(sc.nextLine());
+            int num = Integer.valueOf(sca.nextLine());
             if(num==0){
                 break;
             }
